960caed3c6523aab56238b0201dcd22fa5ba25c8
jicksta <jicksta@21f00dfd-7224-0410-ba9b-b8926ece8ce5>
2007-11-21 06:25:36 +0000

    Using the gem dependency sexiness in test_helper.rb to report that rubigen is a dependency. Thanks Justin Grammens.
    git-svn-id: http://svn.adhearsion.com/trunk@612 21f00dfd-7224-0410-ba9b-b8926ece8ce5

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -19,6 +19,7 @@ require_or_report_dependency('test/spec', 'test-spec')
 require_or_report_dependency('flexmock/test_unit', 'flexmock')
 require_or_report_dependency('active_support', 'activesupport')
 require_or_report_dependency('ruby-debug', 'ruby-debug')
+require_or_report_dependency('rubigen', 'rubigen')
 
 require 'pp'
 require 'stringio'
