26a714becfa896d02f8c09d4f0ab7d0d5b8a11e3
jicksta <jicksta@21f00dfd-7224-0410-ba9b-b8926ece8ce5>
2008-02-18 08:00:07 +0000

    Getting rid of some useless tests
    git-svn-id: http://svn.adhearsion.com/trunk@672 21f00dfd-7224-0410-ba9b-b8926ece8ce5

--- spec/voip/abstractions/test_conference.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../../test_helper'
-describe 'The Conference class' do
-  
-end
-
-describe 'The ConferenceMember class' do
-  
-end
--- spec/voip/abstractions/test_pbx.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../../test_helper'
-
-describe 'PBX' do
-  %w"iax_peers sip_peers".each do |property|
-    test "should expose a #{property}() property"
-  end
-  test "should return an Array of Peer objects"
-end
