fd6888b2cf66305200e5d4f825468fad3e46d3e4
Jay Phillips <jicksta+git@gmail.com>
2008-05-30 21:33:57 -0700

    Fixing indentation issue.

--- spec/test_initialization.rb
+++ spec/test_initialization.rb
@@ -21,43 +21,43 @@ context "Adhearsion::Initializer" do
        flexmock(File).should_receive(:open).once.with(File.join(path, 'adhearsion.pid'), 'w', Proc)
        ahn = Adhearsion::Initializer.new path, :pid_file => true
        ahn.pid_file[0, path.length].should.equal(path)
-     end
-   end
+    end
+  end
    
-   test "should NOT create a pid file in the app's path when given 'false' as the pid_file hash key argument" do
-     with_new_initializer_with_no_path_changing_behavior do
-       ahn = Adhearsion::Initializer.new path, :pid_file => false
-       assert_nil ahn.pid_file
-     end
-   end
-   
-   test "should create a pid file in the app's path by default when daemonizing" do
-     with_new_initializer_with_no_path_changing_behavior do
-       flexmock(File).should_receive(:open).once.with(File.join(path, 'adhearsion.pid'), 'w', Proc)
-       ahn = Adhearsion::Initializer.new path, :daemon => true
-       ahn.pid_file[0, path.size].should.equal(path)
-     end
-   end
-   
-   test "should NOT create a pid file in the app's path when daemonizing and :pid_file is given as false" do
-     with_new_initializer_with_no_path_changing_behavior do
-       ahn = Adhearsion::Initializer.new path, :daemon => true, :pid_file => false
-       assert_nil ahn.pid_file
-     end
-   end
-   
-   test "should create a designated pid file when supplied a String path as :pid_file" do
-     random_file = "/tmp/AHN_TEST_#{rand 100000}.pid"
-     with_new_initializer_with_no_path_changing_behavior do
-       ahn = Adhearsion::Initializer.new path, :pid_file => random_file
-       ahn.pid_file.should.equal(random_file)
-       assert File.exists?(random_file)
-       File.delete random_file
-     end
-   end
-   
-   private
-     def path
+  test "should NOT create a pid file in the app's path when given 'false' as the pid_file hash key argument" do
+    with_new_initializer_with_no_path_changing_behavior do
+      ahn = Adhearsion::Initializer.new path, :pid_file => false
+      assert_nil ahn.pid_file
+    end
+  end
+  
+  test "should create a pid file in the app's path by default when daemonizing" do
+    with_new_initializer_with_no_path_changing_behavior do
+      flexmock(File).should_receive(:open).once.with(File.join(path, 'adhearsion.pid'), 'w', Proc)
+      ahn = Adhearsion::Initializer.new path, :daemon => true
+      ahn.pid_file[0, path.size].should.equal(path)
+    end
+  end
+  
+  test "should NOT create a pid file in the app's path when daemonizing and :pid_file is given as false" do
+    with_new_initializer_with_no_path_changing_behavior do
+      ahn = Adhearsion::Initializer.new path, :daemon => true, :pid_file => false
+      assert_nil ahn.pid_file
+    end
+  end
+  
+  test "should create a designated pid file when supplied a String path as :pid_file" do
+    random_file = "/tmp/AHN_TEST_#{rand 100000}.pid"
+    with_new_initializer_with_no_path_changing_behavior do
+      ahn = Adhearsion::Initializer.new path, :pid_file => random_file
+      ahn.pid_file.should.equal(random_file)
+      assert File.exists?(random_file)
+      File.delete random_file
+    end
+  end
+  
+  private
+    def path
        '/any/ole/path'
      end
 end
