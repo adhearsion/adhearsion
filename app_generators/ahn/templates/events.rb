ad9b16d7e98e9a4e4989d61ce9c1f2032eb65315
Jay Phillips <jicksta+git@gmail.com>
2008-06-03 11:32:18 -0700

    Modified the template events.rb file and made the events.rb file load last in the initialization process

--- app_generators/ahn/templates/events.rb
+++ app_generators/ahn/templates/events.rb
@@ -1,6 +1,6 @@
 # For a full description of what this file does, view the online
 # documentation at http://adhearsion.com
 
-events.framework.before_call.each do |event|
+events.asterisk.before_call.each do |event|
   
 end
