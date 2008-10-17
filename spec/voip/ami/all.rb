92d3d446230df1e607ed583704ff030424975f81
Jay Phillips <jicksta+git@gmail.com>
2008-10-17 15:02:54 -0700

    File to run just the AMI tests now works again.

--- spec/voip/ami/all.rb
+++ spec/voip/ami/all.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), "spec_helper")
+require File.join(File.dirname(__FILE__), "ami_helper")
 Dir[File.join(File.dirname(__FILE__), "test_*")].each do |file|
   require file
 end
