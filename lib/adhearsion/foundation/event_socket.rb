ea48d25e818f10307dca51c89b78fc79a71dbb77
Jay Phillips <jicksta+git@gmail.com>
2008-11-06 14:46:36 -0800

    EventSocket now working great

--- lib/adhearsion/foundation/event_socket.rb
+++ lib/adhearsion/foundation/event_socket.rb
@@ -3,33 +3,59 @@
 # It acts as a Thread-safe Finite State Machine.
 #
 # The handler object has these methods called on it:
-#  - post_init
+#  - connected
 #  - disconnected
 #  - receive_data
+#
+# Note: the EventSocket's state will be changed before these callbacks are executed.
 #
 # The various states are:
 #  - :new
 #  - :connected
 #  - :stopped
 #  - :connection_dropped
-#  - 
 #
+# When instantiating this EventSocket with a block, you register callbacks by defining the methods on the yielded object.
+# For example:
+#   EventSocket.new do |handler|
+#     def handler.receive_data(data)
+#       # Do something here
+#     end
+#     def handler.disconnected(data)
+#       # Do something here
+#     end
+#     def handler.connected(data)
+#       # Do something here
+#     end
+#   end
+#
+require "thread"
+require "socket"
+
 class EventSocket
 
-  MAX_CHUNK_SIZE = 256.kilobytes
+  class << self
+    def connect(*args, &block)
+      instance = new(*args, &block)
+      instance.connect!
+      instance
+    end
+  end
+
+  MAX_CHUNK_SIZE = 256 * 1024
   
   def initialize(host, port, handler=nil, &block)
     raise ArgumentError, "Cannot supply both a handler object and a block" if handler && block_given?
     raise ArgumentError, "Must supply either a handler object or a block" if !handler && !block_given?
     
-    
     @state_lock = Mutex.new
     @host  = host
     @port  = port
     
-    self.state = :new
+    @state = :new
     
     @handler = handler || new_handler_from_block(&block)
+    
   end
   
   def state
@@ -37,39 +63,118 @@ class EventSocket
   end
   
   def connect!
-    @socket = TCPSocket.new(@host, @port)
-    self.state = :connected
-    @handler.post_init
-    reader_loop = Thread.new(&method(:reader_loop))
-  rescue Errno::ECONNREFUSED
-    self.state = :failed
+    @state_lock.synchronize do
+      if @state.equal? :connected
+        raise ConnectionError, "Already connected!"
+      else
+        @socket = TCPSocket.new(@host, @port)
+        @state  = :connected
+      end
+    end  
+    @handler.connected
+    @reader_thread = spawn_reader_thread
+  rescue => error
+    @state = :failed
+    raise error
   end
   
   ##
-  # 
+  # Thread-safe implementation of write.
+  #
+  # @param [String] data Data to write
+  #
+  def write(data)
+    # Note: TCPSocket#write is intrinsically Thread-safe
+    @socket.write data
+  rescue
+    connection_dropped!
+  end
+  
+  ##
+  # Disconnects this EventSocket and sets the state to :stopped
   #
   def disconnect!
-    raise NotImplementedError
+    @state_lock.synchronize do
+      @socket.close rescue nil
+      @state = :stopped
+    end
+  end
+  
+  ##
+  # Joins the Thread which reads data off the socket.
+  #
+  def join
+    @state_lock.synchronize do
+      if @state.equal? :connected
+        @reader_thread.join
+      else
+        nil
+      end
+    end
   end
   
   protected
   
-  def state=(new_state)
-    @state_lock.synchronize { @state = new_state }
+  def connection_dropped!
+    puts "got conneciton dropped"
+    @state_lock.synchronize do
+      unless @state.equal? :connection_dropped
+        @state = :connection_dropped
+        @handler.disconnected
+      end
+    end
+  end
+  
+  def spawn_reader_thread
+    Thread.new(&method(:reader_loop))
   end
   
   def reader_loop
     until state.equal? :stopped
-      @handler.receive_data @socket.readpartial(MAX_CHUNK_SIZE)
+      data = @socket.readpartial(MAX_CHUNK_SIZE)
+      @handler.receive_data data
     end
   rescue EOFError
-    self.state = :connection_dropped
-    @handler.unbind
+    connection_dropped!
+  rescue => e
+    puts "SOME OTHER ERROR?"
   end
 
-  def new_handler_from_block(&block)
-    raise NotImplementedError
+  def new_handler_from_block(&handler_block)
+    handler = Object.new
+    handler.metaclass.send :attr_accessor, :set_callbacks
+    handler.set_callbacks = {:receive_data => false, :disconnected => false, :connected => false }
+    
+    def handler.receive_data(&block)
+      self.metaclass.send :remove_method, :receive_data
+      self.metaclass.send :define_method, :receive_data, &block
+      set_callbacks[:receive_data] = true
+    end
+    def handler.connected(&block)
+      self.metaclass.send :remove_method, :connected
+      self.metaclass.send :define_method, :connected, &block
+      set_callbacks[:connected] = true
+    end
+    def handler.disconnected(&block)
+      self.metaclass.send :remove_method, :disconnected
+      self.metaclass.send :define_method, :disconnected, &block
+      set_callbacks[:disconnected] = true
+    end
+    
+    def handler.singleton_method_added(name)
+      set_callbacks[name.to_sym] = true
+    end
+    
+    yield handler
+    
+    handler.set_callbacks.each_pair do |callback_name,was_set|
+      handler.send(callback_name) {} unless was_set
+    end
+    
+    handler
+    
   end
 
+  class ConnectionError < Exception; end
   
 end
--- spec/foundation/test_event_socket.rb
+++ spec/foundation/test_event_socket.rb
@@ -25,12 +25,49 @@ context "a new EventSocket" do
     EventSocket.new("localhost", 1234, mock_handler_object).state.should.equal :new
   end
   
+  test "the handler created when instantiating the EventSocket with a block" do
+    event_socket = EventSocket.new("foo", 123) do |handler|
+      handler.receive_data { throw :inside_receive_data }
+      handler.disconnected { throw :inside_disconnected}
+      handler.connected { throw :inside_connected }
+    end
+    handler = event_socket.send(:instance_variable_get, :@handler)
+    %w[receive_data disconnected connected].each do |callback|
+      the_following_code {
+        handler.send callback
+      }.should.throw "inside_#{callback}".to_sym
+    end
+  end
+  
+  test 'the handler created when instantiating the EventSocket with a block in which "def" is used to define callbacks' do
+    event_socket = EventSocket.new("foo", 123) do |handler|
+      def handler.receive_data
+        throw :inside_receive_data
+      end
+      def handler.disconnected
+        throw :inside_disconnected
+      end
+      def handler.connected
+        throw :inside_connected
+      end
+    end
+    handler = event_socket.send(:instance_variable_get, :@handler)
+    %w[receive_data disconnected connected].each do |callback|
+      the_following_code {
+        handler.send callback
+      }.should.throw "inside_#{callback}".to_sym
+    end
+  end
+  
 end
-
 
 context "connecting an EventSocket" do
   
   include EventSocketTestHelper
+  
+  before :each do
+    flexmock(Mutex).new_instances.should_receive(:synchronize).zero_or_more_times.and_yield
+  end
   
   test "should instantiate a new TCPSocket with the correct host and port" do
     host, port = "google.com", 80
@@ -48,12 +85,15 @@ context "connecting an EventSocket" do
     event_socket.state.should.equal :connected
   end
   
-  test "should have a state of :failed when ECONNREFUSED is raised" do
+  test "should have a state of :failed after ECONNREFUSED is raised" do
     host, port = "google.com", 80
-    mock_socket = flexmock "TCPSocket"
     flexmock(TCPSocket).should_receive(:new).once.with(host, port).and_raise(Errno::ECONNREFUSED)
     event_socket = EventSocket.new(host, port, mock_handler_object)
-    event_socket.connect!
+    
+    the_following_code {
+      event_socket.connect!
+    }.should.raise Errno::ECONNREFUSED
+    
     event_socket.state.should.equal :failed
   end
   
@@ -69,11 +109,55 @@ context "connecting an EventSocket" do
   
 end
 
+context "the reader_loop method" do
+  
+  include EventSocketTestHelper
+  
+  test "should call readpartial() on the TCPSocket and pass that to the receive_data method of handler" do
+    mock_handler = flexmock "mock handler object"
+    mock_socket  = flexmock "TCPSocket"
+    data         = "Jay Phillips " * 25
+    
+    mock_handler.should_receive(:connected).and_return
+    mock_handler.should_receive(:disconnected).and_return
+    mock_handler.should_receive(:receive_data).once.with(data).and_throw :done_testing
+    
+    mock_socket.should_receive(:readpartial).once.with(EventSocket::MAX_CHUNK_SIZE).and_return data
+    
+    flexmock(TCPSocket).should_receive(:new).once.and_return mock_socket
+    flexmock(Thread).should_receive(:new).and_yield
+    
+    the_following_code {
+      EventSocket.new("foo", 123, mock_handler).connect!
+    }.should.throw :done_testing
+  end
+  
+  test "should set the state to :connection_dropped when an EOFError is raised by readpartial" do
+    mock_socket  = flexmock "TCPSocket"
+    mock_socket.should_receive(:readpartial).once.and_raise EOFError
+    flexmock(TCPSocket).should_receive(:new).once.and_return mock_socket
+    
+    flexmock(Thread).should_receive(:new).and_yield
+    
+    event_socket = EventSocket.new("foo", 123, mock_handler_object)
+    
+    flexmock(event_socket).should_receive(:state=).once.with(:connected)
+    flexmock(event_socket).should_receive(:state=).once.with(:connection_dropped)
+    
+    event_socket.connect!
+  end
+  
+end
+
+context "the writer_loop method" do
+  
+end
+
 BEGIN {
   module EventSocketTestHelper
     def mock_handler_object
       returning flexmock do |handler|
-        flexstub(handler).should_receive(:post_init)
+        flexstub(handler).should_receive(:connected)
         flexstub(handler).should_receive(:receive_data)
         flexstub(handler).should_receive(:disconnected)
       end
