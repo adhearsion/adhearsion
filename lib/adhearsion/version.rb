2210e397af32e4cd78ee211b5bbb720490ed2463
Jay Phillips <jicksta+git@gmail.com>
2008-12-26 22:48:14 -0800

    Incrementing version to 0.8.0!!!

--- lib/adhearsion/version.rb
+++ lib/adhearsion/version.rb
@@ -1,8 +1,8 @@
 module Adhearsion #:nodoc:
   module VERSION #:nodoc:
-    MAJOR = 0   unless defined? MAJOR
-    MINOR = 7   unless defined? MINOR
-    TINY  = 999 unless defined? TINY
+    MAJOR = 0 unless defined? MAJOR
+    MINOR = 8 unless defined? MINOR
+    TINY  = 0 unless defined? TINY
 
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined? STRING
   end
