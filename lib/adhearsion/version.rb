cf7529b8baab97afcfb10a22bbc5857d3241eba2
Ben Klang <ben@alkaloid.net>
2010-09-03 13:55:31 -0400

    Version bump: 0.8.7

--- lib/adhearsion/version.rb
+++ lib/adhearsion/version.rb
@@ -2,7 +2,7 @@ module Adhearsion #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0 unless defined? MAJOR
     MINOR = 8 unless defined? MINOR
-    TINY  = 6 unless defined? TINY
+    TINY  = 7 unless defined? TINY
 
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined? STRING
   end
