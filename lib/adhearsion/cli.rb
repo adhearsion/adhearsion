31fc36cec74f892e5bcc1a86429c4a8d1b6f1f01
Ben Langfeld <ben@langfeld.me>
2011-07-28 13:45:27 +0100

    First pass at forking if running CLI within an Adhearsion app

--- lib/adhearsion/cli.rb
+++ lib/adhearsion/cli.rb
@@ -1,1 +1,7 @@
+require 'adhearsion/script_ahn_loader'
+
+# If we are inside an Adhearsion application this method performs an exec and thus
+# the rest of this script is not run.
+Adhearsion::ScriptAhnLoader.exec_script_ahn!
+
 require 'adhearsion/commands'
