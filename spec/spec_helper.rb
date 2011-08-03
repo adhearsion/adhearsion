5d132621801d0b9ad35ea3702f56a2869c45f0fd
Ben Langfeld <ben@langfeld.me>
2011-08-03 10:18:13 +0100

    Just use the simplecov Rcov formatter for now to make CI happy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
 require 'simplecov'
 require 'simplecov-rcov'
-class SimpleCov::Formatter::MergedFormatter
-  def format(result)
-     SimpleCov::Formatter::HTMLFormatter.new.format(result)
-     SimpleCov::Formatter::RcovFormatter.new.format(result)
-  end
-end
-SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start do
   add_filter "/vendor/"
   add_filter "/spec/"
