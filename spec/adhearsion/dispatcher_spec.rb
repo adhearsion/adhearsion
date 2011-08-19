beef8a8f917388e2835a83f73d25fb663b1a7bea
Ben Langfeld <ben@langfeld.me>
2011-08-19 12:23:02 +0100

    Clean up dispatcher spec a little

--- spec/adhearsion/dispatcher_spec.rb
+++ spec/adhearsion/dispatcher_spec.rb
@@ -6,21 +6,22 @@ module Adhearsion
       Adhearsion::Events.reinitialize_theatre!
     end
 
-    let(:call_id)     { rand }
-    let(:mock_offer)  { flexmock 'Offer', :call_id => call_id }
-    let(:mock_call)   { flexmock 'Call', :id => call_id }
-    let(:mock_client) { flexmock 'Client' }
+    let(:call_id)       { rand }
+    let(:mock_offer)    { flexmock 'Offer', :call_id => call_id }
+    let(:mock_call)     { flexmock 'Call', :id => call_id }
+    let(:mock_client)   { flexmock 'Client' }
+    let(:mock_manager)  { flexmock 'a mock dialplan manager' }
 
     subject { Dispatcher.new mock_client }
 
     describe "dispatching an offer" do
       it 'should hand the call off to a new Manager' do
         flexmock(Adhearsion).should_receive(:receive_call_from).once.and_return mock_call
-        manager_mock = flexmock 'a mock dialplan manager'
-        manager_mock.should_receive(:handle).once.with(mock_call)
-        flexmock(DialPlan::Manager).should_receive(:new).once.and_return manager_mock
-        subject.dispatch_offer mock_offer
+        mock_manager.should_receive(:handle).once.with(mock_call)
+        flexmock(DialPlan::Manager).should_receive(:new).once.and_return mock_manager
       end
+
+      after { subject.dispatch_offer mock_offer }
     end
   end
 end
