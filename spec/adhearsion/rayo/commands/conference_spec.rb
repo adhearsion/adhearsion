afdd40c9dcc6a611fcfaaf478364ba977c401314
Ben Langfeld <ben@langfeld.me>
2011-08-30 17:24:41 +0100

    Add ultra-basic conferencing support

--- spec/adhearsion/rayo/commands/conference_spec.rb
+++ spec/adhearsion/rayo/commands/conference_spec.rb
@@ -6,6 +6,15 @@ module Adhearsion
       describe Conference do
         include RayoCommandTestHelpers
 
+        describe "#conference" do
+          let(:conference_id) { 'abc123' }
+          let(:options)       { { :mute => true } }
+
+          it 'executes a Conference with the correct options' do
+            expect_component_execution Punchblock::Component::Tropo::Conference.new(options.merge(:name => conference_id))
+            mock_execution_environment.conference(conference_id, options).should be true
+          end
+        end
       end
     end
   end
