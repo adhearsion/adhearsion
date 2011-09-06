ffa8893b4937e6374515f463d3226381fcb828c6
Julius F <baldrailers@gmail.com>
2011-09-06 18:54:35 +0800

    Adding base record component for punchblock

--- lib/adhearsion/rayo/commands/record.rb
+++ lib/adhearsion/rayo/commands/record.rb
@@ -2,7 +2,13 @@ module Adhearsion
   module Rayo
     module Commands
       module Record
-
+        #
+        # Start docs here
+        #
+        def record(format, options = [], &block)
+          options.merge! :format => format
+          execute_component_and_await_completion Punchblock::Component::Record.new(options), &block
+        end# record(text, options = [], &block)
       end
     end
   end
--- spec/adhearsion/rayo/commands/record_spec.rb
+++ spec/adhearsion/rayo/commands/record_spec.rb
@@ -6,6 +6,16 @@ module Adhearsion
       describe Record do
         include RayoCommandTestHelpers
 
+        describe "#record" do
+          let(:format) { 'mp3' }
+          let(:options) { {:start_beep => true} }
+
+          it 'executes a Record with plain text/string input' do
+            expect_component_execution Punchblock::Component::Record.new(options.merge(:format => format))
+            mock_execution_environment.record(format, options).should be true
+          end
+        end
+
       end
     end
   end
