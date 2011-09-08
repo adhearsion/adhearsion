d58c08696e8bf869471c09e91d4080672d2eef39
Ben Langfeld <ben@langfeld.me>
2011-09-08 17:16:27 +0100

    Test skeleton for Commands#dial

--- spec/adhearsion/rayo/commands/dial_spec.rb
+++ spec/adhearsion/rayo/commands/dial_spec.rb
@@ -6,6 +6,21 @@ module Adhearsion
       describe Dial do
         include RayoCommandTestHelpers
 
+        describe "#dial" do
+          describe "without a block" do
+            it "blocks the original dialplan until the new call hangs up"
+
+            it "joins the new call to the existing one"
+          end
+
+        	describe "with a block" do
+            it "uses the block as the dialplan for the new call"
+
+            it "joins the new call to the existing call once the block returns"
+
+            it "does not try to join the calls if the new call is hungup when the block returns"
+          end
+        end
       end
     end
   end
