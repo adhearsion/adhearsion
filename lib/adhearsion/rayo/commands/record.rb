7f12da5357b5c9d2fd6cd6925c0bd12ea708c121
Julius F <baldrailers@gmail.com>
2011-09-10 07:39:40 +0800

    Adding complete filters and non-async filter

--- lib/adhearsion/rayo/commands/record.rb
+++ lib/adhearsion/rayo/commands/record.rb
@@ -3,14 +3,21 @@ module Adhearsion
     module Commands
       module Record
 
-        def record(options = {})
+        def record(options = {}, &block)
           async = options[:async]? true : false
           on_complete = options[:on_complete]
           options.delete :async
           options.delete :on_complete
 
-          options.merge! :event_callback => lambda { |event| on_complete.call(event.recording) }
-          execute_component_and_await_completion Punchblock::Component::Record.new(options)
+          # record :async => true
+          # Provides recording case for voicemail system
+          if async
+            options.merge! :event_callback => lambda { |event| on_complete.call(event.recording) if event.is_a? Punchblock::Event::Complete }
+            execute_component_and_await_completion Punchblock::Component::Record.new(options)
+          else
+            evented = execute_component_and_await_completion Punchblock::Component::Record.new(options)
+            block.call(evented.complete_event.resource.recording)
+          end
         end# record(async, options = {}, &block)
 
       end
