35528fe3ccdd8fdfc5e2acfe152ae7a5eca182b6
Julius F <baldrailers@gmail.com>
2011-09-09 16:32:58 +0800

    Adding event_callback handler for record method

--- lib/adhearsion/rayo/commands/record.rb
+++ lib/adhearsion/rayo/commands/record.rb
@@ -9,16 +9,8 @@ module Adhearsion
           options.delete :async
           options.delete :on_complete
 
-          if async
-            # Executes the dialplan with the given parameters most commonly used for voicemail applications
-            # Accepts a :terminator digit to end the recording
-            options.merge! :event_callback => lambda { |recording| on_complete }
-            execute_component_and_await_completion Punchblock::Component::Record.new(options), &on_complete
-          else
-            # Executes the dialplan with the given parameters most commonly used for supervision mode.
-            # Accepts keypress/DTMF to pause/unpause the recording
-            execute_component_and_await_completion Punchblock::Component::Record.new(options)
-          end
+          options.merge! :event_callback => lambda { |event| on_complete.call(event.recording) }
+          execute_component_and_await_completion Punchblock::Component::Record.new(options)
         end# record(async, options = {}, &block)
 
       end
