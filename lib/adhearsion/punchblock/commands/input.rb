27ddd7621c3337896361d83c730c3b96418d9ced
Luca Pradovera <luca.pradovera@gmail.com>
2011-09-27 00:41:37 +0200

    Moved all input grammars to GRXML. Added a grammar_digits utility method to Input.

--- lib/adhearsion/punchblock/commands/input.rb
+++ lib/adhearsion/punchblock/commands/input.rb
@@ -2,7 +2,27 @@ module Adhearsion
   module Punchblock
     module Commands
       module Input
+        # Utility method for DTMF GRXML grammars
+        #
+        # @param [Integer] Number of digits to accept in the grammar.
+        # @return [RubySpeech::GRXML::Grammar] A grammar suitable for use in SSML prompts.
+        def grammar_digits(digits = 1)
+          grammar = RubySpeech::GRXML.draw do
+            self.mode = 'dtmf'
+            self.root = 'inputdigits'
+            rule id: 'digits' do
+              one_of do
+                0.upto(9) {|d| item { d.to_s } }
+              end
+            end
 
+            rule id: 'inputdigits', scope: 'public' do
+              item repeat: digits.to_s do
+                ruleref uri: '#digits'
+              end
+            end
+          end
+        end#grammar_digits
       end
     end
   end
