a7daf3bd4778dbf72a7b6e9d2e17a8d9dda03524
Ben Langfeld <ben@langfeld.me>
2011-10-12 16:42:07 +0100

    Include methods_for(:events) in the correct module

--- lib/adhearsion/initializer.rb
+++ lib/adhearsion/initializer.rb
@@ -279,7 +279,7 @@ Adhearsion will abort until you fix this. Sorry for the incovenience.
         Components.component_manager = Components::ComponentManager.new @components_directory
         Kernel.send(:const_set, :COMPONENTS, Components.component_manager.lazy_config_loader)
         Components.component_manager.globalize_global_scope!
-        Components.component_manager.extend_object_with(Theatre::CallbackDefinitionLoader, :events)
+        Components.component_manager.extend_object_with(Events, :events)
       else
         ahn_log.warn "No components directory found. Not initializing any components."
       end
