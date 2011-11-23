a185d3f97b68e5b6316abfa54418361dd4c55e84
Ben Klang <bklang@mojolingo.com>
2011-11-23 17:06:14 -0500

    [FEATURE] Complete 3-phase shutdown steps
    On first :shutdown, we flag the state internally.  The intent is to shut down when the active calls count reaches 0, but otherwise operate normally.
    
    On second :shutdown, we start rejecting new incoming calls.  Existing calls will continue to process until completion.  Shut down when active call count reaches 0.
    
    On third :shutdown, send a Hangup to all active calls.  Shut down when active call count reaches 0.
    
    In addition, the process can be force-stopped, which simply closes the connection to the server (and any component connections as well).

--- lib/adhearsion/process.rb
+++ lib/adhearsion/process.rb
@@ -7,7 +7,8 @@ module Adhearsion
 
     state_machine :initial => :booting do
       before_transition :log_state_change
-      after_transition :on => :shutdown, :do => :trigger_shutdown_events
+      after_transition :on => :shutdown, :do => :request_stop
+      after_transition :on => :stopped, :do => :final_shutdown
 
       event :booted do
         transition :booting => :running
@@ -17,11 +18,19 @@ module Adhearsion
         transition :running => :stopping
       end
 
+      # On second shutdown request, start rejecting new calls.
+      # This corresponds to the admin pressing CTRL+C twice.
       event :shutdown do
         transition :stopping => :rejecting
       end
 
-      event :final_shutdown do
+      # On third shutdown request, hang up all active calls.
+      # This corresponds to the admin pressing CTRL+C three times.
+      event :shutdown do
+        transition :rejecting => :stopped
+      end
+
+      event :hard_shutdown do
         transition [:running, :stopping] => :rejecting
       end
 
@@ -29,7 +38,7 @@ module Adhearsion
         transition :rejecting => :stopped
       end
 
-      event :hard_stop do
+      event :force_stop do
         transition all => :stopped
       end
 
@@ -44,10 +53,17 @@ module Adhearsion
 
     def log_state_change(transition)
       event, from, to = transition.event, transition.from_name, transition.to_name
-      puts "Adhearsion transitioning from #{from} to #{to}."
+      logger.info "Adhearsion transitioning from #{from} to #{to} with #{Adhearsion.active_calls.size} active calls."
     end
 
-    def trigger_shutdown_events
+    def request_stop
+      Events.trigger_immediately :stop_requested
+    end
+
+    def final_shutdown
+      Adhearsion.active_calls.each do |call|
+        call.hangup rescue nil
+      end
       Events.trigger_immediately :shutdown
     end
 
--- spec/adhearsion/process_spec.rb
+++ spec/adhearsion/process_spec.rb
@@ -5,9 +5,19 @@ describe Adhearsion::Process do
     Adhearsion::Process.reset
   end
 
-  it 'should trigger :shutdown events on shutdown' do
-    flexmock(Adhearsion::Events).should_receive(:trigger_immediately).once.with(:shutdown)
+  it 'should trigger :stop_requested events on shutdown' do
+    flexmock(Adhearsion::Events).should_receive(:trigger_immediately).once.with(:stop_requested)
     Adhearsion::Process.booted
     Adhearsion::Process.shutdown
+  end
+
+  it 'should trigger :shutdown events on force_stop' do
+    pending 'How to test after_transition events?'
+    flexmock(Adhearsion::Events).should_receive(:trigger_immediately).once.with(:shutdown)
+    Adhearsion::Process.force_stop
+  end
+
+  it 'should send a hangup to all active calls on force_stop' do
+    pending
   end
 end
