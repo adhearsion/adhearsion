2af321249bb458873403f6e7851de1eb914b5dda
Ben Klang <bklang@mojolingo.com>
2011-11-23 10:10:29 -0500

    [CS] Remove trailing whitespace

--- lib/adhearsion/process.rb
+++ lib/adhearsion/process.rb
@@ -4,23 +4,23 @@ require 'singleton'
 module Adhearsion
   class Process
     include Singleton
-    
+
     state_machine :initial => :booting do
       before_transition :log_state_change
       after_transition :on => :shutdown, :do => :trigger_shutdown_events
-      
+
       event :booted do
         transition :booting => :running
       end
-      
+
       event :shutdown do
         transition :running => :stopping
       end
-      
+
       event :final_shutdown do
         transition [:running, :stopping] => :rejecting
       end
-      
+
       event :stop do
         transition :rejecting => :stopped
       end
@@ -32,17 +32,18 @@ module Adhearsion
       def trigger_shutdown_events
         Events.trigger_immediately :shutdown
       end
+
     end
-    
+
     def initialize
       super
     end
-    
+
     def log_state_change(transition)
       event, from, to = transition.event, transition.from_name, transition.to_name
       puts "Adhearsion transitioning from #{from} to #{to}."
     end
-    
+
     def self.method_missing(method_name, *args, &block)
       instance.send method_name, *args, &block
     end
