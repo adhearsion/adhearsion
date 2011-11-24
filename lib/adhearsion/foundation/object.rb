b7dc22ca469b240dba8a315387da27b01403a9e3
Ben Langfeld <ben@langfeld.me>
2011-11-24 13:13:03 +0000

    [CS] Remove definition of Object#tap since all supported Ruby platforms support it natively

--- lib/adhearsion/foundation/object.rb
+++ lib/adhearsion/foundation/object.rb
@@ -1,16 +1,5 @@
 require 'adhearsion/logging'
 
-# Monkey patch Object to support the #tap method.
-# This method is present in Ruby 1.8.7 and later.
-unless Object.respond_to?(:tap)
-  class Object
-    def tap
-      yield self
-      self
-    end
-  end
-end
-
 class Object
   def pb_logger
     logger
