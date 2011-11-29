c01e1dd6193b3ff2fc01b73bc424271d61a5d0c8
juandebravo <juandebravo@gmail.com>
2011-11-29 11:41:15 -0500

    load punchblock_plugin configuration in configuration tasks

--- lib/adhearsion/tasks/configuration.rb
+++ lib/adhearsion/tasks/configuration.rb
@@ -1,18 +1,24 @@
-desc "Show configuration values"
+require 'adhearsion/punchblock_plugin'
+
+begin
+  require "#{Dir.pwd}/config/startup.rb"
+rescue Exception => ex
+  STDERR.puts "\nCannot find the Adhearsion application startup file: #{ex}"
+end
 
 namespace :adhearsion do
 
   namespace :config do
 
-    desc "Show configuration values in STDOUT"
+    desc "Show configuration values in STDOUT; it accepts a parameter: [nil|platform|<plugin-name>|all]"
     task :show, :name do |t, args|
-      name = args.name.nil? ? :platform : args.name.to_sym
+      name = args.name.nil? ? :all : args.name.to_sym
       puts Adhearsion.config.description name, :show_values => true
     end
 
-    desc "Show configuration description options in STDOUT"
+    desc "Show configuration description in STDOUT; it accepts a parameter: [nil|platform|<plugin-name>|all]"
     task :desc, :name do |t, args|
-      name = args.name.nil? ? :platform : args.name.to_sym
+      name = args.name.nil? ? :all : args.name.to_sym
       puts Adhearsion.config.description name, :show_values => false
     end
   end
