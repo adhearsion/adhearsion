eb071a6aebae84dfae78abc9e9e15fd7854a6dc1
juandebravo <juandebravo@gmail.com>
2011-11-29 11:41:31 -0500

    punchblock plugin configuration

--- lib/adhearsion/punchblock_plugin.rb
+++ lib/adhearsion/punchblock_plugin.rb
@@ -7,19 +7,20 @@ module Adhearsion
     autoload :Initializer
 
     config :punchblock do
-      platform       :xmpp            , :desc => "Platform punchblock shall use to connect to the Telephony provider"
-      username       "usera@127.0.0.1", :desc => "Authentication credentials"
-      password       "1"              , :desc => "Authentication credentials"
-      auto_reconnect true             , :desc => "Autoreconnect in case of failure"
-      wire_logger    nil              , :desc => ""
-      transport_logger nil            , :desc => ""
+      platform         :xmpp            , :desc => <<-__
+        Platform punchblock shall use to connect to the Telephony provider. Currently supported values:
+        - :xmpp
+        - :asterisk
+      __
+      username         "usera@127.0.0.1", :desc => "Authentication credentials"
+      password         "1"              , :desc => "Authentication credentials"
+      auto_reconnect   true             , :desc => "Autoreconnect in case of failure"
+      wire_logger      nil              , :desc => ""
+      transport_logger nil              , :desc => ""
     end
 
     init :punchblock do
-      if Adhearsion.config[:punchblock] && Adhearsion.config[:punchblock].enabled
-        logger.debug "Initializing Punchblock using PunchblockPlugin"
-        Initializer.start
-      end
+      Initializer.start
     end
     
   end
