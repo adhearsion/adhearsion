8b57e4aaa7e81a7a70cab51b08d994853879a0f3
Ben Langfeld <ben@langfeld.me>
2011-12-13 15:02:05 +0000

    [BUGFIX] Fix errors with the menu DSL specs using the call controller helpers
    * Also remove duplicated specs

--- spec/adhearsion/punchblock/menu_dsl_spec.rb
+++ spec/adhearsion/punchblock/menu_dsl_spec.rb
@@ -3,14 +3,11 @@ require 'spec_helper'
 module Adhearsion
   module Punchblock
     describe MenuDSL do
-      include CallControllerTestHelpers
-
       it "should accept hash options"
 
       it "should accept hash options and block"
 
       it "should accept hash options and block and evaluate block"
-
     end
   end
 end
