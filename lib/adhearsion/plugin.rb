07a05e900057c6f5e5c427afaf0150de6e8b3c5b
Ben Langfeld <ben@langfeld.me>
2011-12-23 14:43:12 +0000

    [CS] Fix some indenting

--- lib/adhearsion/plugin.rb
+++ lib/adhearsion/plugin.rb
@@ -145,15 +145,16 @@ module Adhearsion
       #
       # * Example 1:
       #
-      #    FooBar = Class.new Adhearsion::Plugin do
-      #      tasks do
-      #        namespace :foo_bar do
-      #        desc "Prints the FooBar plugin version"
-      #        task :version do
-      #          STDOUT.puts "FooBar plugin v0.1"
-      #        end
-      #      end
-      #    end
+      #     FooBar = Class.new Adhearsion::Plugin do
+      #       tasks do
+      #         namespace :foo_bar do
+      #           desc "Prints the FooBar plugin version"
+      #           task :version do
+      #             STDOUT.puts "FooBar plugin v0.1"
+      #           end
+      #         end
+      #       end
+      #     end
       #
       # * Example 2:
       #
@@ -165,12 +166,12 @@ module Adhearsion
       #
       #    = tasks/foo_bar.rake
       #
-      #    namespace :foo_bar do
+      #     namespace :foo_bar do
       #       desc "Prints the FooBar plugin version"
       #       task :version do
       #         STDOUT.puts "FooBar plugin v0.1"
       #       end
-      #    end
+      #     end
       #
       def tasks
         @@rake_tasks << Proc.new if block_given?
