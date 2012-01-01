84c94d4a4cf85b88a56ba644ac86d79b41712e35
Taylor Carpenter <taylor@codecafe.com>
2012-01-01 05:11:38 -0600

    [UPDATE] Upstream has merged terminate code via pull request

--- features/support/aruba_helper.rb
+++ features/support/aruba_helper.rb
@@ -1,31 +1,10 @@
 module Aruba
   module Api
-    def terminate_processes!
-      processes.each do |_, process|
-        terminate_process(process)
-        stop_process(process)
-      end
-    end
-
-    def terminate_process(process)
-      process.terminate(@aruba_keep_ansi)
-    end
-
     # output() blocks for stderr and stdout it seems
     def interactive_stdout_contains(expected, actual)
       if @interactive
         @interactive.stdout(@aruba_keep_ansi)
         unescape(actual).include?(unescape(expected)) ? true : false
-      end
-    end
-  end
-
-  class Process
-    def terminate(keep_ansi)
-      if @process
-        stdout(keep_ansi) && stderr(keep_ansi) # flush output
-        @process.stop
-        stdout(keep_ansi) && stderr(keep_ansi) # flush output
       end
     end
   end
