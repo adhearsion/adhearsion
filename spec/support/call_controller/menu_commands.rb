d32fc5b46675799f4f891601f7cca53ccb433f1a
Luca Pradovera <luca.pradovera@gmail.com>
2012-01-04 14:29:56 +0100

    Controller #menu tests

--- spec/support/call_controller/menu_commands.rb
+++ spec/support/call_controller/menu_commands.rb
@@ -11,6 +11,15 @@ module Adhearsion
           subject.should_receive(:interruptible_play).with(sound_file).and_return("1")
           subject.play_sound_files_for_menu(menu_instance, sound_files)
         end
+
+        it "should wait for digit if nothing is pressed during playback" do
+          subject.should_receive(:interruptible_play).with(sound_file).and_return(nil)
+          subject.should_receive(:wait_for_digit).with(menu_instance.timeout).and_return("1")
+        end
+      end#play_sound_files_for_menu
+
+      describe "#menu" do
+        
       end
 
     end#shared
