1f785caebdbb435b5eba3630a3f00b1280bc9ce1
juandebravo <juandebravo@gmail.com>
2012-01-15 12:22:59 +0100

    update environment var RAILS_ENV if required
    This is done in the initialization process before
    init plugins
    
    * AHN_ENV set to valid value:
      * RAILS_ENV set and != AHN_ENV => RAILS_ENV = AHN_ENV
      * RAILS_ENV set and == AHN_ENV => RAILS_ENV not updated
    
    * AHN_ENV unset:
      * RAILS_ENV set: RAILS_ENV not updated
      * RAILS_ENV unset: RAILS_ENV set to 
         Adhearsion.config.platform.environment (default development)

--- spec/support/initializer_stubs.rb
+++ spec/support/initializer_stubs.rb
@@ -1,6 +1,6 @@
 module InitializerStubs
   UNWANTED_BEHAVIOR = {
-    Adhearsion::Initializer => [:initialize_log_file, :switch_to_root_directory, :daemonize!, :require, :init_plugins, :load_lib_folder]
+    Adhearsion::Initializer => [:initialize_log_file, :update_rails_env_var, :switch_to_root_directory, :daemonize!, :require, :init_plugins, :load_lib_folder]
   } unless defined? UNWANTED_BEHAVIOR
 
   def stub_behavior_for_initializer_with_no_path_changing_behavior
