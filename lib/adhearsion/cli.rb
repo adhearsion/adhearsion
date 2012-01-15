212c7644acbd8f686c400e12f85edb9cadc9c22c
Taylor Carpenter <taylor@codecafe.com>
2012-01-15 10:14:36 -0600

    [UPDATE] simplifying ahn script just a bit

--- lib/adhearsion/cli.rb
+++ lib/adhearsion/cli.rb
@@ -1,7 +1,6 @@
-require 'adhearsion/script_ahn_loader'
+require 'adhearsion/cli_commands'
 
 # If we are inside an Adhearsion application this method performs an exec and thus
 # the rest of this script is not run.
 Adhearsion::ScriptAhnLoader.exec_script_ahn!
-
-require 'adhearsion/cli_commands'
+Adhearsion::CLI::AhnCommand.execute!
