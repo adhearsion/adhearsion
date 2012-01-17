b3761aeb061b75467befd1c4482a0e0d3b26e7ee
Ben Langfeld <ben@langfeld.me>
2012-01-17 14:06:38 +0000

    [BUGFIX] Fix CLI start mode parsing
    * ahn start will start the app in the foreground with a console
    * ahn daemon will daemonize the app with an optional PID file
    * Cleaner missing command failure

--- lib/adhearsion/cli_commands.rb
+++ lib/adhearsion/cli_commands.rb
@@ -5,9 +5,6 @@ require 'thor'
 module Adhearsion
   module CLI
     class AhnCommand < Thor
-      TRY_HELP_MSG = "\nTry: #{File.basename($0)} help"
-      VALID_START_MODES = %w(daemon console)
-
       map %w(-h --h -help --help) => :help
       map %w(-v --v -version --version) => :version
 
@@ -16,64 +13,31 @@ module Adhearsion
       desc "create /path/to/directory", "Create a new Adhearsion application under the given path"
       def create(path)
         require 'adhearsion/generators'
-        require 'adhearsion/generators/app/app_generator.rb'
+        require 'adhearsion/generators/app/app_generator'
         Adhearsion::Generators::AppGenerator.start
       end
 
       desc "version", "Shows Adhearsion version"
       def version
         say "Adhearsion v#{Adhearsion::VERSION}"
-        exit(0)
+        exit 0
       end
 
-      desc "start [console|daemon] </path/to/directory>", "Start the Adhearsion server"
-      long_desc <<-DESC
-      Start the Adhearsion server
-
-          console -- starts the interactive Adhearsion CLI\n
-          daemon -- runs the server in the background
-      DESC
-      method_option :console, :type => :boolean
-      method_option :daemon, :type => :boolean
-      method_option :pidfile, :type => :string, :aliases => %w(--pid-file)
+      desc "start </path/to/directory>", "Start the Adhearsion server in the foreground with a console"
       def start(*args)
-        mode = nil
-        pid_file = options[:pidfile]
-        puts "ARGV: #{ARGV.join(' ')}"
-        puts "ARGV SIZE: #{ARGV.size}"
-        puts "args SIZE: #{args.size}"
-        puts options.inspect
-        puts "pid file #{pid_file.inspect}"
-        #puts "path: #{path.inspect}"
-        puts "args: #{args.inspect}"
-        puts "mode: #{mode.inspect}"
+        start_app args.first, :console
+      end
 
-        raise CLIException, "Invalid start mode requested!" + TRY_HELP_MSG + " start" \
-          if options[:console] and options[:daemon]
+      desc "daemon </path/to/directory>", "Start the Adhearsion server in the background"
+      method_option :pidfile, :type => :string, :aliases => %w(--pid-file)
+      def daemon(*args)
+        start_app args.first, :daemon, options[:pidfile]
+      end
 
-        mode = if options[:console]
-                 :console
-               elsif options[:daemon]
-                 :daemon
-               else
-                 :foreground
-               end
+      protected
 
-        puts "mode: #{mode.inspect}"
-
-        puts "args.first #{args.first}"
-
-        if args.size > 2
-          raise CLIException, "Too many arguments supplied!" + TRY_HELP_MSG + " start" \
-        elsif args == 2 and args.first =~ /foreground|daemon|console/
-          path = args.last
-          mode = args.first.to_sym
-        elsif args == 1
-          path = args.last
-        else
-          raise CLIException, "Invalid start mode requested: #{args.first}"
-        end
-
+      def start_app(path, mode, pid_file = nil)
+        path ||= '.'
         execute_from_app_dir!(path, ARGV) unless
           ScriptAhnLoader.in_ahn_application? or
           ScriptAhnLoader.in_ahn_application_subdirectory?
@@ -82,9 +46,9 @@ module Adhearsion
         Adhearsion::Initializer.start path, :mode => mode, :pid_file => pid_file
       end
 
-      protected
       def method_missing(action, *args)
-        raise UnknownCommand, [action, *args, TRY_HELP_MSG] * " "
+        help
+        raise UnknownCommand, [action, *args] * " "
       end
 
       def execute_from_app_dir!(app_path, *args)
