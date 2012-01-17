a2fa4eded9752afb6a48ae6d0e3082f2eb839f1a
Ben Klang <bklang@mojolingo.com>
2012-01-17 14:35:51 -0500

    Fix invoking validators with Loquacious

--- lib/adhearsion/punchblock_plugin.rb
+++ lib/adhearsion/punchblock_plugin.rb
@@ -17,18 +17,20 @@ module Adhearsion
       root_domain         nil              , :desc => "The root domain at which to address the server"
       calls_domain        nil              , :desc => "The domain at which to address calls"
       mixers_domain       nil              , :desc => "The domain at which to address mixers"
-      connection_timeout  60               , :transform => Proc.new {|v| validate_number(v)}, :desc => "The amount of time to wait for a connection"
-      reconnect_attempts  1.0/0.0          , :transform => Proc.new {|v| validate_number(v)}, :desc => "The number of times to (re)attempt connection to the server"
-      reconnect_timer     5                , :transform => Proc.new {|v| validate_number(v)}, :desc => "Delay between connection attempts"
+      connection_timeout  60               , :transform => Proc.new { |v| PunchblockPlugin.validate_number(v)}, :desc => "The amount of time to wait for a connection"
+      reconnect_attempts  1.0/0.0          , :transform => Proc.new { |v| PunchblockPlugin.validate_number(v)}, :desc => "The number of times to (re)attempt connection to the server"
+      reconnect_timer     5                , :transform => Proc.new { |v| PunchblockPlugin.validate_number(v)}, :desc => "Delay between connection attempts"
     end
 
     init :punchblock do
       Initializer.start
     end
 
-    def validate_number(value)
-      return 1.0/0.0 if ["Infinity", 1.0/0.0].include? value
-      value.to_i
+    class << self
+      def validate_number(value)
+        return 1.0/0.0 if ["Infinity", 1.0/0.0].include? value
+        value.to_i
+      end
     end
   end
 end
