17dedd67b48bbf2b04421e727dcf90e532d7aec2
Ben Langfeld <ben@langfeld.me>
2012-01-17 17:14:38 +0000

    [BUGFIX] Update initialiser specs for no more path option

--- spec/adhearsion/initializer_spec.rb
+++ spec/adhearsion/initializer_spec.rb
@@ -5,9 +5,7 @@ describe Adhearsion::Initializer do
   include InitializerStubs
   # TODO: create a specification for aliases
 
-  let :path do
-    '/any/ole/path/'
-  end
+  let(:path) { "#{Dir.pwd}/" }
 
   describe "#start" do
     before do
@@ -28,23 +26,23 @@ describe Adhearsion::Initializer do
       Adhearsion::Logging.silence!
     end
 
-    it "initialization will start with only a path given" do
+    it "initialization will start with no options" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
-        Adhearsion::Initializer.start path
+        Adhearsion::Initializer.start
       end
     end
 
     it "should create a pid file in the app's path when given 'true' as the pid_file hash key argument" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
          flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-         ahn = Adhearsion::Initializer.start path, :pid_file => true
+         ahn = Adhearsion::Initializer.start :pid_file => true
          ahn.pid_file[0, path.length].should == path
       end
     end
 
     it "should NOT create a pid file in the app's path when given 'false' as the pid_file hash key argument" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
-        ahn = Adhearsion::Initializer.start path, :pid_file => false
+        ahn = Adhearsion::Initializer.start :pid_file => false
         ahn.pid_file.should be nil
       end
     end
@@ -52,14 +50,14 @@ describe Adhearsion::Initializer do
     it "should create a pid file in the app's path by default when daemonizing" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
         flexmock(File).should_receive(:open).once.with(File.join(path, 'adhearsion.pid'), 'w', Proc)
-        ahn = Adhearsion::Initializer.start path, :mode => :daemon
+        ahn = Adhearsion::Initializer.start :mode => :daemon
         ahn.pid_file[0, path.size].should == path
       end
     end
 
     it "should NOT create a pid file in the app's path when daemonizing and :pid_file is given as false" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
-        ahn = Adhearsion::Initializer.start path, :daemon => true, :pid_file => false
+        ahn = Adhearsion::Initializer.start :daemon => true, :pid_file => false
         ahn.pid_file.should be nil
       end
     end
@@ -67,8 +65,8 @@ describe Adhearsion::Initializer do
     it "should create a designated pid file when supplied a String path as :pid_file" do
       random_file = "/tmp/AHN_TEST_#{rand 100000}.pid"
       stub_behavior_for_initializer_with_no_path_changing_behavior do
-        ahn = Adhearsion::Initializer.start path, :pid_file => random_file
-        ahn.pid_file.should be(random_file)
+        ahn = Adhearsion::Initializer.start :pid_file => random_file
+        ahn.pid_file.should == random_file
         File.exists?(random_file).should be true
         File.delete random_file
       end
@@ -77,7 +75,7 @@ describe Adhearsion::Initializer do
     it "should resolve the log file path to daemonize" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
          flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-         ahn = Adhearsion::Initializer.start path, :pid_file => true
+         ahn = Adhearsion::Initializer.start :pid_file => true
          ahn.resolve_log_file_path.should == path + Adhearsion.config.platform.logging.outputters[0]
       end
     end
@@ -86,7 +84,7 @@ describe Adhearsion::Initializer do
       Adhearsion.config.platform.logging.outputters = ["log/my_application.log", "log/adhearsion.log"]
       stub_behavior_for_initializer_with_no_path_changing_behavior do
          flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-         ahn = Adhearsion::Initializer.start path, :pid_file => true
+         ahn = Adhearsion::Initializer.start :pid_file => true
          ahn.resolve_log_file_path.should == path + Adhearsion.config.platform.logging.outputters[0]
       end
     end
@@ -94,7 +92,7 @@ describe Adhearsion::Initializer do
     it "should return a valid appenders array" do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
          flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-         ahn = Adhearsion::Initializer.start path, :pid_file => true
+         ahn = Adhearsion::Initializer.start :pid_file => true
          appenders = ahn.init_get_logging_appenders
          appenders.should have(2).items
          appenders[1].should be_instance_of Logging::Appenders::Stdout
@@ -104,7 +102,7 @@ describe Adhearsion::Initializer do
     it "should initialize properly the log paths" do
       ahn = stub_behavior_for_initializer_with_no_path_changing_behavior do
         flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-        Adhearsion::Initializer.start path, :pid_file => true
+        Adhearsion::Initializer.start :pid_file => true
       end
       flexmock(Dir).should_receive(:mkdir).with("log/")
       ahn.initialize_log_paths
@@ -114,7 +112,7 @@ describe Adhearsion::Initializer do
       Adhearsion.config.platform.logging.outputters = ["log/my_application.log", "log/test/adhearsion.log"]
       ahn = stub_behavior_for_initializer_with_no_path_changing_behavior do
         flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
-        Adhearsion::Initializer.start path, :pid_file => true
+        Adhearsion::Initializer.start :pid_file => true
       end
       flexmock(Dir).should_receive(:mkdir).with("log/").twice
       flexmock(Dir).should_receive(:mkdir).with("log/test/").once
@@ -141,7 +139,7 @@ describe Adhearsion::Initializer do
       stub_behavior_for_initializer_with_no_path_changing_behavior do
         flexmock(File).should_receive(:open).with(File.join(path, 'adhearsion.pid'), 'w', Proc).at_least.once
         flexmock(Adhearsion::Initializer::Logging).should_receive(:start).once.with(Array, :info, nil).and_return('')
-        Adhearsion::Initializer.start path, :pid_file => true
+        Adhearsion::Initializer.start :pid_file => true
       end
     end
   end
@@ -152,54 +150,24 @@ describe Adhearsion::Initializer do
     end
 
     it "should load the contents of lib directory" do
-      flexmock(Dir).should_receive(:chdir).with("/any/ole/path/lib", Proc).and_return []
-      Adhearsion::Initializer.new(path).load_lib_folder
+      flexmock(Dir).should_receive(:chdir).with(File.join(path, "lib"), Proc).and_return []
+      Adhearsion::Initializer.new.load_lib_folder
     end
 
     it "should return false if folder does not exist" do
-      Adhearsion::Initializer.new(path).load_lib_folder.should == false
+      Adhearsion.config.platform.lib = "my_random_lib_directory"
+      Adhearsion::Initializer.new.load_lib_folder.should == false
     end
 
     it "should return false and not load any file if config folder is set to nil" do
       Adhearsion.config.platform.lib = nil
-      Adhearsion::Initializer.new(path).load_lib_folder.should == false
+      Adhearsion::Initializer.new.load_lib_folder.should == false
     end
 
     it "should load the contents of the preconfigured directory" do
       Adhearsion.config.platform.lib = "foo"
       flexmock(Dir).should_receive(:chdir).with("/any/ole/path/foo", Proc).and_return []
-      Adhearsion::Initializer.new(path).load_lib_folder
-    end
-  end
-
-  describe ".ahn_root" do
-
-    before do
-      Adhearsion.ahn_root = nil
-    end
-
-    it "initializing will create the ahn_root" do
-      flexmock(Adhearsion::Initializer::Logging).should_receive(:start).once.and_return('')
-      flexmock(::Logging::Appenders::File).should_receive(:assert_valid_logfile).and_return(true)
-      flexmock(::Logging::Appenders).should_receive(:file).and_return(nil)
-
-      stub_behavior_for_initializer_with_no_path_changing_behavior do
-        ahn = Adhearsion::Initializer.start path
-        Adhearsion.config.root.should_not be_nil
-      end
-    end
-
-    it "creating the Adhearsion.config.root will set defaults" do
-      flexmock(Adhearsion::Initializer::Logging).should_receive(:start).once.and_return('')
-      flexmock(::Logging::Appenders::File).should_receive(:assert_valid_logfile).and_return(true)
-      flexmock(::Logging::Appenders).should_receive(:file).and_return(nil)
-
-      stub_behavior_for_initializer_with_no_path_changing_behavior do
-        flexstub(Adhearsion::Initializer).new_instances.should_receive(:load).and_return
-        ahn = Adhearsion::Initializer.start path
-        full_path = File.expand_path(path)
-        Adhearsion.config.root.to_s.should == full_path
-      end
+      Adhearsion::Initializer.new.load_lib_folder
     end
   end
 end
@@ -225,7 +193,7 @@ describe "Updating RAILS_ENV variable" do
         ahn = nil
         stub_behavior_for_initializer_with_no_path_changing_behavior do
           Adhearsion.config.platform.environment = env
-          ahn = Adhearsion::Initializer.start '/any/ole/path'
+          ahn = Adhearsion::Initializer.start
         end
         ahn.update_rails_env_var
         ENV['RAILS_ENV'].should == env.to_s
@@ -243,7 +211,7 @@ describe "Updating RAILS_ENV variable" do
         ENV['RAILS_ENV'] = "test"
         ahn = nil
         stub_behavior_for_initializer_with_no_path_changing_behavior do
-          ahn = Adhearsion::Initializer.start '/any/ole/path'
+          ahn = Adhearsion::Initializer.start
         end
         ahn.update_rails_env_var
         ENV['RAILS_ENV'].should == "test"
@@ -254,7 +222,7 @@ describe "Updating RAILS_ENV variable" do
         ENV['AHN_ENV'] = "production"
         ahn = nil
         stub_behavior_for_initializer_with_no_path_changing_behavior do
-          ahn = Adhearsion::Initializer.start '/any/ole/path'
+          ahn = Adhearsion::Initializer.start
         end
         ahn.update_rails_env_var
         ENV['RAILS_ENV'].should == "production"
@@ -271,7 +239,7 @@ describe "Updating RAILS_ENV variable" do
         ENV['AHN_ENV'] = "production"
         ahn = nil
         stub_behavior_for_initializer_with_no_path_changing_behavior do
-          ahn = Adhearsion::Initializer.start '/any/ole/path'
+          ahn = Adhearsion::Initializer.start
         end
         ahn.update_rails_env_var
         ENV['RAILS_ENV'].should == "production"
