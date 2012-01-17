40829a20026f00e5ed137016c3fde168829c1cef
Ben Klang <bklang@mojolingo.com>
2012-01-17 13:54:13 -0500

    [FEAT] Enable ENV variable function

--- lib/adhearsion/configuration.rb
+++ lib/adhearsion/configuration.rb
@@ -14,6 +14,7 @@ module Adhearsion
     def initialize(&block)
       initialize_environments
 
+      Loquacious.env_config = true
       Loquacious.env_prefix = "AHN"
 
       Loquacious::Configuration.for :platform do
