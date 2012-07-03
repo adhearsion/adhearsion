e2d8af7f24dc912e08bd72ffb8950b61f64d9062
Stephen George <sgit-turtle.testudo12@wronghead.com>
2012-07-03 08:32:37 -0500

    [CS] make spec for escaped tts string unambiguous
    Prefer '\\"' over '\"' for clarity (they are equivalent)

--- spec/adhearsion/voip/asterisk/commands_spec.rb
+++ spec/adhearsion/voip/asterisk/commands_spec.rb
@@ -3005,7 +3005,7 @@ describe "speak command" do
     end
 
     it "should properly escape double-quotes (for XML) in the TTS string" do
-      mock_call.should_receive(:raw_response).once.with('EXEC MRCPSynth "<speak xmlns=\\\\\"http://www.w3.org/2001/10/synthesis\\\\\" version=\\\\\"1.0\\\\\" xml:lang=\\\\\"en-US\\\\\"> <voice name=\\\\\"Paul\\\\\"> <prosody rate=\\\\\"1.0\\\\\">Howdy, stranger. How are you today?</prosody> </voice> </speak>"').and_return pbx_success_response
+      mock_call.should_receive(:raw_response).once.with('EXEC MRCPSynth "<speak xmlns=\\\\\\"http://www.w3.org/2001/10/synthesis\\\\\\" version=\\\\\\"1.0\\\\\\" xml:lang=\\\\\\"en-US\\\\\\"> <voice name=\\\\\\"Paul\\\\\\"> <prosody rate=\\\\\\"1.0\\\\\\">Howdy, stranger. How are you today?</prosody> </voice> </speak>"').and_return pbx_success_response
       @speech_engines.unimrcp(mock_call, '<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US"> <voice name="Paul"> <prosody rate="1.0">Howdy, stranger. How are you today?</prosody> </voice> </speak>')
     end
 
