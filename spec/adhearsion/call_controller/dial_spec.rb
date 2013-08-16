9c7c751c0e11fd63685dc8f3eaf466a52a0199d8
Ben Langfeld <ben@langfeld.me>
2013-08-16 13:33:35 -0300

    [CS] Avoid another dial spec race

--- spec/adhearsion/call_controller/dial_spec.rb
+++ spec/adhearsion/call_controller/dial_spec.rb
@@ -1156,7 +1156,9 @@ module Adhearsion
             end
 
             it "should join the calls if the call is still active after execution of the call controller" do
-              other_mock_call.should_receive(:hangup).once
+              other_mock_call.should_receive(:hangup).once.and_return do
+                other_mock_call << mock_end
+              end
               other_mock_call['confirm'] = true
               call.should_receive(:answer).once
               other_mock_call.should_receive(:join).once.with(call)
@@ -1173,7 +1175,6 @@ module Adhearsion
               base_time = Time.local(2008, 9, 1, 12, 0, 42)
               Timecop.freeze base_time
               other_mock_call << Punchblock::Event::Unjoined.new(call_uri: call.id)
-              other_mock_call << mock_end
 
               latch.wait(1).should be_true
 
