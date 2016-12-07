ebfa5dc58aba4516100c8c7d213f2a366736ce3b
G-Bianchi <gggg.bianchi@gmail.com>
2016-12-07 18:38:21 +0100

    Invoke terminating! method on call end event

--- lib/adhearsion/call.rb
+++ lib/adhearsion/call.rb
@@ -245,6 +245,7 @@ module Adhearsion
 
       on_end do |event|
         logger.info "Call #{from} -> #{to} ended due to #{event.reason}#{" (code #{event.platform_code})" if event.platform_code}"
+        terminating!
         @end_time = event.timestamp.to_time
         @duration = @end_time - @start_time if @start_time
         clear_from_active_calls
