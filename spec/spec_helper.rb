b70d7b06294dd02c044927f599e7eaad598bbe5a
David Wilkie <dwilkie@gmail.com>
2022-02-16 09:56:50 +0700

    Configure lcov path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ if ENV.key?("CI")
   require "simplecov-lcov"
   SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
   SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov::Formatter::LcovFormatter.config.single_report_path = './coverage/lcov.info'
 end
 
 %w{
